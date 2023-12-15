fe5eb27aa191079aa604b966800587493d8a09c5
Kim Seongji <22000097@handong.ac.kr>
2023-12-16 01:35:08 +0900

    test

--- src/main/java/com/example/board/BoardController.java
+++ src/main/java/com/example/board/BoardController.java
@@ -7,11 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import com.example.board.FileUpload;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.util.UUID;
 
 @Controller
 public class BoardController {
--- src/main/java/com/example/util/LoginCheckInterceptor.java
+++ src/main/java/com/example/util/LoginCheckInterceptor.java
@@ -2,7 +2,6 @@ package com.example.util;
 
 
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
